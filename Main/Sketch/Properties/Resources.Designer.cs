13611db96d77b6b74b04362c38dc79a7b24a1b25
Владислав Ширинкин <vladislav.shirinkin@outlook.com>
2015-12-25 17:33:01 +0500

    ссылки и библиотеки

--- Main/Sketch/Properties/Resources.Designer.cs
+++ Main/Sketch/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Sketch.Properties {
+namespace Sketches.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Sketch.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sketch.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sketches.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Main/Sketch/Properties/Settings.Designer.cs
+++ Main/Sketch/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Sketch.Properties {
+namespace Sketches.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
